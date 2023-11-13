3495b0f874edbfe8592c7a688ce905bc0bd464bf
KennethMcLachlan <kennethmclachlan11@gmail.com>
2023-11-12 20:20:49 -0800

    Player jump and flip completed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,9 +24,14 @@ public class Player : MonoBehaviour
 
     private CharacterController _controller;
 
+    private Animator _anim;
+
+    private bool _jumping;
+
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
+        _anim = GetComponentInChildren<Animator>();
     }
     private void Update()
     {
@@ -35,16 +40,41 @@ public class Player : MonoBehaviour
 
     private void CalculateMovement()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
         _direction = new Vector3(horizontalInput, 0, 0);
         Vector3 velocity = _direction * _speed;
 
+        _anim.SetFloat("Speed", Mathf.Abs(horizontalInput));
+
+        //what direction to face?
+        //if direction on the x is greater than 0
+        //then face right
+        //else
+        //face 
+
+        if (horizontalInput != 0)
+        {
+            Vector3 facing = transform.localEulerAngles;
+            facing.y = _direction.x > 0 ? 0 : 180; ;
+            transform.localEulerAngles = facing;
+        }
+        
 
         if (_controller.isGrounded == true)
         {
+            if (_jumping == true)
+            {
+                _jumping = false;
+                _anim.SetBool("Jumping", _jumping);
+            }
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _yVelocity = _jumpHeight;
+
+                //_anim.SetTrigger("RunningJump");
+                _jumping = true;
+                _anim.SetBool("Jumping", _jumping);
+
                 _canDoubleJump = true;
             }
         }
